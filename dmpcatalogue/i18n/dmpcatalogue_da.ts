337ee66d4fc61472069239cd28e41f5d261873b5
Alexander Bruy <alexander.bruy@gmail.com>
2023-05-16 19:24:16 +0300

    strings update

--- dmpcatalogue/i18n/dmpcatalogue_da.ts
+++ dmpcatalogue/i18n/dmpcatalogue_da.ts
@@ -4,95 +4,130 @@
 <context>
     <name>CatalogueDockWidget</name>
     <message>
-        <location filename="../gui/dock_widget.py" line="196"/>
+        <location filename="../gui/dock_widget.py" line="370"/>
         <source>DMP Catalogue</source>
         <translation type="unfinished">DMP Datakatalog</translation>
     </message>
     <message>
-        <location filename="../ui/catalogue_widget.ui" line="54"/>
+        <location filename="../ui/catalogue_widget.ui" line="129"/>
         <source>Search…</source>
         <translation type="unfinished">Søg…</translation>
     </message>
     <message>
-        <location filename="../ui/catalogue_widget.ui" line="72"/>
+        <location filename="../ui/catalogue_widget.ui" line="151"/>
         <source>Options…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/catalogue_widget.ui" line="77"/>
-        <source>Filters</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../gui/dock_widget.py" line="49"/>
+        <location filename="../gui/dock_widget.py" line="88"/>
         <source>All</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="56"/>
+        <location filename="../gui/dock_widget.py" line="94"/>
         <source>OWS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="62"/>
+        <location filename="../gui/dock_widget.py" line="101"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="91"/>
+        <location filename="../gui/dock_widget.py" line="128"/>
         <source>File downloaded </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="122"/>
-        <source>Add as {protocol.upper()} layer</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../gui/dock_widget.py" line="131"/>
-        <source>Download…</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../gui/dock_widget.py" line="135"/>
+        <location filename="../gui/dock_widget.py" line="187"/>
         <source>Remove from Favorite</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="135"/>
+        <location filename="../gui/dock_widget.py" line="187"/>
         <source>Add to Favorite</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="143"/>
+        <location filename="../gui/dock_widget.py" line="250"/>
         <source>Details…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="153"/>
+        <location filename="../gui/dock_widget.py" line="321"/>
         <source>Dataset has no layers.</source>
         <translation type="unfinished">Datasættet har ikke nogen lag.</translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="157"/>
+        <location filename="../gui/dock_widget.py" line="325"/>
         <source>Failed to load layer: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="168"/>
+        <location filename="../gui/dock_widget.py" line="335"/>
         <source>Save File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dock_widget.py" line="168"/>
-        <source>All Files (*.*)</source>
+        <location filename="../ui/catalogue_widget.ui" line="39"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Collections are groups of layers that are&lt;br&gt;frequently used together. Collections are&lt;br&gt;tailored towards specific job functions and&lt;br&gt;areas, and are maintained by working&lt;br&gt;groups and domain experts.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/catalogue_widget.ui" line="46"/>
+        <source>Datasets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/catalogue_widget.ui" line="97"/>
+        <source>Collections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/catalogue_widget.ui" line="167"/>
+        <source>Datasources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/catalogue_widget.ui" line="178"/>
+        <source>Group by owners</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="227"/>
+        <source>Add</source>
+        <translation type="unfinished">Tilføj</translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="232"/>
+        <source>{protocol.upper()} layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="244"/>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="293"/>
+        <source>There are no layers in the dataset </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="299"/>
+        <source>Failed to load </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/dock_widget.py" line="335"/>
+        <source>ZIP Archives (*.zip *.ZIP)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>DataRegistry</name>
     <message>
-        <location filename="../core/data_registry.py" line="102"/>
+        <location filename="../core/data_registry.py" line="201"/>
         <source>Network request failed: </source>
         <translation type="unfinished"></translation>
     </message>
@@ -108,12 +143,12 @@
 <context>
     <name>DatasetItemModel</name>
     <message>
-        <location filename="../gui/dataset_item_model.py" line="270"/>
+        <location filename="../gui/dataset_item_model.py" line="348"/>
         <source>Status:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/dataset_item_model.py" line="300"/>
+        <location filename="../gui/dataset_item_model.py" line="380"/>
         <source>Favorites</source>
         <translation type="unfinished"></translation>
     </message>
@@ -121,58 +156,103 @@
 <context>
     <name>DetailsDialog</name>
     <message>
-        <location filename="../gui/details_dialog.py" line="50"/>
+        <location filename="../gui/details_dialog.py" line="152"/>
         <source>General</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="52"/>
+        <location filename="../gui/details_dialog.py" line="154"/>
         <source>Title</source>
         <translation type="unfinished">Titel</translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="54"/>
+        <location filename="../gui/details_dialog.py" line="57"/>
         <source>Category</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="56"/>
+        <location filename="../gui/details_dialog.py" line="158"/>
         <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="60"/>
+        <location filename="../gui/details_dialog.py" line="65"/>
         <source>Tags</source>
         <translation type="unfinished">Tags</translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="65"/>
+        <location filename="../gui/details_dialog.py" line="70"/>
         <source>Owners</source>
         <translation type="unfinished">Ejere</translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="71"/>
+        <location filename="../gui/details_dialog.py" line="76"/>
         <source>Additional information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="73"/>
+        <location filename="../gui/details_dialog.py" line="79"/>
         <source>Support contact</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="79"/>
+        <location filename="../gui/details_dialog.py" line="87"/>
         <source>Metadata</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="85"/>
+        <location filename="../gui/details_dialog.py" line="94"/>
         <source>Created</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/details_dialog.py" line="87"/>
+        <location filename="../gui/details_dialog.py" line="96"/>
         <source>Updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="101"/>
+        <source>WMS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="131"/>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="133"/>
+        <source>Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="136"/>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="140"/>
+        <source>Format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="117"/>
+        <source>WFS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="122"/>
+        <source>Type name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="128"/>
+        <source>WMTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/details_dialog.py" line="138"/>
+        <source>Tile matrix</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -284,7 +364,7 @@
 <context>
     <name>DmpPlugin</name>
     <message>
-        <location filename="../plugin.py" line="79"/>
+        <location filename="../plugin.py" line="105"/>
         <source>DMP Catalogue</source>
         <translation>DMP Datakatalog</translation>
     </message>
